97a6903aceff4e555294ea068bc94f1acaf46cac
Araceli Ramos <celi2455@gmail.com>
2021-09-19 14:38:01 -0500

    Completed assignment

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+    array = ["I", "love", "learning", "wow"]
+    array.sort
 end
 
 def using_reverse(array)
-
+    array = ["wow", 2, 3, "arrays!"]
+    array.reverse
 end
 
 def using_first(array)
-
+    array = ["wow", 2, 3, 4]
+    array.first
 end
 
 def using_last(array)
-
+    array = [1, 2, 3, "arrays!"]
+    array.last
 end
 
 def using_size(array)
-
+    array = [1, 2, 3, 4, 5, 6]
+    array.size
 end
